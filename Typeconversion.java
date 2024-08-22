eecf1a3e8de4a93ef07337b0e4dbf82c2f475511
santhakumariuchula <151708630+santhakumariuchula@users.noreply.github.com>
2024-08-22 11:56:46 +0530

    Add files via upload

---
+++ Typeconversion.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class Typeconversion{
+    public static void main(String args[])
+    {
+        double z;
+        z=(double)22/7;
+        System.out.println("z value is"+z);
+    }
+}
