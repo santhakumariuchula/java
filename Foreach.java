f054aa79e58cfe203c2f44696c32772a473e7c4a
santhakumariuchula <151708630+santhakumariuchula@users.noreply.github.com>
2024-09-05 11:30:54 +0530

    Add files via upload

---
+++ Foreach.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Foreach
+{
+    public static void main(String args[])
+    {
+        int arr[]={10,20,30,40,50};
+        for(int i:arr)
+        System.out.println(i+" ");
+    }
+}
