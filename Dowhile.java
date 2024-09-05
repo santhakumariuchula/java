f6737b557d978be3ba658915b0c161939583e93a
santhakumariuchula <151708630+santhakumariuchula@users.noreply.github.com>
2024-09-05 11:18:52 +0530

    Add files via upload

---
+++ Dowhile.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class Dowhile
+{
+    public static void main(String args[])
+    {
+        int i=0;
+        do{
+            System.out.println("Helloworld");
+            i++;
+        }
+        while(i>3);
+    }
+}
