753a6eca938d23ca15e51a42c2a1958279c24eb8
santhakumariuchula <151708630+santhakumariuchula@users.noreply.github.com>
2024-11-05 14:43:17 +0530

    Add files via upload

---
+++ StringDemo.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.*;
+public class StringDemo{
+    public static void main(String args[]){
+        String s1="welcome";
+        char ch[]={'s','a','n','t','h','i'};
+        String s2=new String(ch);
+        String s3=new String("example");
+        System.out.println("String s1 is"+s1);
+         System.out.println("String s2 is"+s2);
+          System.out.println("String s3 is"+s3);
+    }
+}
